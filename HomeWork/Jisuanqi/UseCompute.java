3dfed9ff6109fac7763b532a9a8a73a1cbb120ee
macong0503 <1512346520@qq.com>
2021-07-23 16:54:21 +0800

    Create UseCompute.java

---
+++ HomeWork/Jisuanqi/UseCompute.java
@@ -0,0 +1,12 @@
+package HomeWork.Jisuanqi;
+
+import HomeWork.Jisuanqi.Add;
+
+public class UseCompute {
+    public void useCom(Compute com, int n, int m){
+        System.out.println(com.computer(n,m));
+
+        }
+
+    }
+
